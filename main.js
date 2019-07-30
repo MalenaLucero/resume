2f8b9f30084954f4e77cfe584e060ba8a1479a5b
MalenaLucero <luceromalena@hotmail.com>
2019-07-30 12:50:41 -0300

    mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,54 @@
-const showMenu = () =>{
-    let menu = document.getElementById('menu')
-    if(menu.style.display = "none"){
-        menu.style.display = "block"
-        menu.classList.replace('show', 'hide')
-    }else{
-        menu.style.display = "none"
-        menu.classList.replace('hide', 'show')
-    }
-    console.log(menu.style.display)
+const home = () =>{
+    showElement('education')
+    showElement('languages')
+    showElement('portfolio')
+    showElement('contact')
+    toggleMenu()
+}
+
+const education = () =>{
+    showElement('education')
+    hideElement('languages')
+    hideElement('portfolio')
+    hideElement('contact')
+    toggleMenu()
+}
+
+const languages = () =>{
+    showElement('languages')
+    hideElement('education')
+    hideElement('portfolio')
+    hideElement('contact')
+    toggleMenu()
+}
+
+const portfolio = () =>{
+    showElement('portfolio')
+    hideElement('languages')
+    hideElement('education')
+    hideElement('contact')
+    toggleMenu()
+}
+
+const contact = () =>{
+    showElement('contact')
+    hideElement('languages')
+    hideElement('portfolio')
+    hideElement('education')
+    toggleMenu()
+}
+
+const showElement = (elementId) =>{
+    let element = document.getElementById(elementId)
+    element.classList.replace('hide', 'show')
+}
+
+const hideElement = (elementId) =>{
+    let element = document.getElementById(elementId)
+    element.classList.replace('show', 'hide')
+}
+
+const toggleMenu = () =>{
+    var menu = document.getElementById('menu')
+    menu.classList.toggle('open')
 }
